4abf11bbfe2127fc2155bfbd9f47ea7d29815fe0
Mohammed W. Hammami <biowesshq@gmail.com>
2026-04-18 14:21:50 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'; // Add hooks
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { AppShell } from './components/AppShell';
 import { EditorPage } from './pages/EditorPage';
 import { FilesPage } from './pages/FilesPage';
